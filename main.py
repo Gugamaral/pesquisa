41b59efe5779e86fc8812df801d49518eac3f6a8
Gustavo Amaral <gugamaral@gmail.com>
2024-05-23 21:22:06 -0300

    Versão 1.1

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 nomes = ['Adão', "Helião", "Chups", 'Carminha', 'Maria Baixinha', 'João Boca Azeda', 'Beltiza', 'Erondina', 'Juninho Lobrega', 'Orizonmarden', 'Zequinha', 'Ninoca', 'Joabe', 'Noemia', 'Rosa', 'Juscelino']
 
 # usuário informa o nome que deseja pesquisar
-nome = input('Digite o nome a ser pesquisado: ')
+nome = input('Digite o nome a ser pesquisado: ').capitalize()
 
 # retorna o índice do nome pesquisado
-indice = nomes.index(nome)
 
 # verifica se o nome está na lista ou não
-if nome in nomes:
+try:
+    indice = nomes.index(nome)
     print(f'Nome encontrado: {nome} no índice {indice}.')
-else:
+except:
     print(f'{nome} não encontrado na lista.')
